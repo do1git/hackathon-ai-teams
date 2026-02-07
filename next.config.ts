9f35ddd8e0411dc22f282a0c89b78b316d14def6
duwon <dev.dothe1@gmail.com>
2026-02-07 15:31:31 +0900

    fix5

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb",
+    experimental: {
+        serverActions: {
+            bodySizeLimit: "10mb",
+        },
     },
-  },
+    transpilePackages: ["@moru-ai/core"],
 };
 
 export default nextConfig;
